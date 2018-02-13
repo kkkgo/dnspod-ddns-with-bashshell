ecb139ebeb6e74264fc421cb53d162fbf693747b
kissshot <k@03k.org>
2018-02-14 00:44:26 +0800

    Update dnspod_ddns.sh

--- dnspod_ddns.sh
+++ dnspod_ddns.sh
@@ -17,7 +17,11 @@ IPREX='([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0
 URLIP=$(curl $(if [ -n "$OUT" ]; then echo "--interface $OUT"; fi) -s $CHECKURL|grep -Eo "$IPREX"|tail -n1)
 echo "[URL IP]:$URLIP"
 dnscmd="nslookup";type nslookup >/dev/null 2>&1||dnscmd="ping -c1"
-DNSIP=$($dnscmd $host.$domain|grep -Eo "$IPREX"|tail -n1)
+DNSTEST=$($dnscmd $DOMAIN)
+if [ "$?" == 0 ];then
+DNSIP=$(echo $DNSTEST|grep -Eo "$IPREX"|tail -n1)
+else DNSIP="Get $DOMAIN DNS Failed."
+fi
 echo "[DNS IP]:$DNSIP"
 if [ "$DNSIP" == "$URLIP" ];then
 echo "IP SAME IN DNS,SKIP UPDATE."
