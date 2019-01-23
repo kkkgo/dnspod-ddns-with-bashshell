b1c36bae78ebca0c263da37cfc847605493f24eb
kissshot <4530897+kkkgo@users.noreply.github.com>
2019-01-23 20:12:05 +0800

    Allow insecure server connections when using SSL

--- dnspod_ddns_line.sh
+++ dnspod_ddns_line.sh
@@ -42,7 +42,7 @@ fi
 record_id=$(echo ${Record#*\"records\"\:\[\{\"id\"}|cut -d'"' -f2)
 record_line_id=$(echo ${Record#*line_id}|cut -d'"' -f3)
 echo Start DDNS update...
-ddns="$(curl -4 $(if [ -n "$OUT" ]; then echo "--interface $OUT"; fi) -s -X POST https://dnsapi.cn/Record.Ddns -d "${token}&record_id=${record_id}&record_line_id=${record_line_id}&value=$DEVIP")"
+ddns="$(curl -4 -k $(if [ -n "$OUT" ]; then echo "--interface $OUT"; fi) -s -X POST https://dnsapi.cn/Record.Ddns -d "${token}&record_id=${record_id}&record_line_id=${record_line_id}&value=$DEVIP")"
 ddns_result="$(echo ${ddns#*message\"}|cut -d'"' -f2)"
 echo -n "DDNS upadte result:$ddns_result "
 echo $ddns|grep -Eo "$IPREX"|tail -n1
