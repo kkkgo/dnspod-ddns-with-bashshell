d4f5b835eef5a30f87c7bf69b561a93a52ffec83
livejq <livejq@qq.com>
2020-04-26 13:03:55 +0800

    修复普通用户ttl无法设置为60，支持泛域名

--- dnspod_ddns.sh
+++ dnspod_ddns.sh
@@ -1,13 +1,15 @@
-#Dnspod DDNS with BashShell
 #Github:https://github.com/kkkgo/dnspod-ddns-with-bashshell
 #More: https://03k.org/dnspod-ddns-with-bashshell.html
 #CONF START
-API_ID=12345
-API_Token=abcdefghijklmnopq2333333
+API_ID=123456789
+API_Token=4213d874131gfdg32412138
 domain=example.com
-host=home
-CHECKURL="http://ip.03k.org"
-#OUT="pppoe"
+host=@
+record_type=A
+ttl=600
+CHECKURL="http://myip.ipip.net"
+#OUT="eth0"
+#Email=example@qq.com
 #CONF END
 . /etc/profile
 date
@@ -19,18 +21,29 @@ URLIP="Get $DOMAIN URLIP Failed."
 fi
 echo "[URL IP]:$URLIP"
 dnscmd="nslookup";type nslookup >/dev/null 2>&1||dnscmd="ping -c1"
-DNSTEST=$($dnscmd $host.$domain)
+if [[ -z $host || $host == '@' ]];then
+URL=$domain
+general=1
+else URL=$($host.$domain)
+general=0
+fi
+echo "[URL]:$URL"
+DNSTEST=$($dnscmd $URL)
 if [ "$?" != 0 ]&&[ "$dnscmd" == "nslookup" ]||(echo $DNSTEST |grep -qEvo "$IPREX");then
-DNSIP="Get $host.$domain DNS Failed."
+DNSIP="Get $URL DNS Failed."
 else DNSIP=$(echo $DNSTEST|grep -Eo "$IPREX"|tail -n1)
 fi
 echo "[DNS IP]:$DNSIP"
 if [ "$DNSIP" == "$URLIP" ];then
-echo "IP SAME IN DNS,SKIP UPDATE."
+echo "IP SAME IN DNS,SKIP MODIFY."
 exit
 fi
 fi
-token="login_token=${API_ID},${API_Token}&format=json&lang=en&error_on_empty=yes&domain=${domain}&sub_domain=${host}"
+
+if [ $general == 0 ];then
+token="login_token=${API_ID},${API_Token}&format=json&domain=${domain}&sub_domain=${host}"
+else token="login_token=${API_ID},${API_Token}&format=json&domain=${domain}"
+fi
 Record="$(curl -4 -k $(if [ -n "$OUT" ]; then echo "--interface $OUT"; fi) -s -X POST https://dnsapi.cn/Record.List -d "${token}")"
 iferr="$(echo ${Record#*code}|cut -d'"' -f3)"
 if [ "$iferr" == "1" ];then
@@ -42,11 +55,12 @@ exit
 fi
 record_id=$(echo ${Record#*\"records\"\:\[\{\"id\"}|cut -d'"' -f2)
 record_line_id=$(echo ${Record#*line_id}|cut -d'"' -f3)
-echo Start DDNS update...
-ddns="$(curl -4 -k $(if [ -n "$OUT" ]; then echo "--interface $OUT"; fi) -s -X POST https://dnsapi.cn/Record.Ddns -d "${token}&record_id=${record_id}&record_line_id=${record_line_id}")"
+echo Start DDNS modify...
+ddns="$(curl -4 -k $(if [ -n "$OUT" ]; then echo "--interface $OUT"; fi) -s -X POST https://dnsapi.cn/Record.Modify -d "${token}&ttl=${ttl}&record_type=${record_type}&value=${URLIP}&record_id=${record_id}&record_line_id=${record_line_id}")"
 ddns_result="$(echo ${ddns#*message\"}|cut -d'"' -f2)"
-echo -n "DDNS upadte result:$ddns_result "
-echo $ddns|grep -Eo "$IPREX"|tail -n1
-else echo -n Get $host.$domain error :
+printf "DDNS upadte result:$ddns_result\n"
+now_record_ip=$(echo $ddns|grep -Eo "$IPREX"|tail -n1)
+printf "[NOW RECORD IP]:${now_record_ip}\n" 
+else echo -n Get $URL error :
 echo $(echo ${Record#*message\"})|cut -d'"' -f2
 fi
